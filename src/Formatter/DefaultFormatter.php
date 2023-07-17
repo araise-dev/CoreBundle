dfaa8399da8cef520067b9c0229832c84f1c3932
tuxes3 <nicolo.singer@gmaill.com>
2023-07-17 17:43:58 +0200

    feat(formatter): use StringConverter

--- src/Formatter/DefaultFormatter.php
+++ src/Formatter/DefaultFormatter.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace araise\CoreBundle\Formatter;
 
+use Coduo\ToString\StringConverter;
+
 class DefaultFormatter extends AbstractFormatter
 {
     public function getString(mixed $value): string
     {
-        return (string) $value;
+        return (string) new StringConverter($value);
     }
 }
